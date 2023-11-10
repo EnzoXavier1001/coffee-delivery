20c4c720286389cbb89385e8ea08fc2c74288a5c
Enzo Costa <enzo_costa7@hotmail.com>
2023-11-10 09:16:37 -0300

    feat: create initial structure

---
+++ src/styles/themes/defaultTheme.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+    white: '#FFF',
+    background: '#FAFAFA',
+    
+    '--yellow-300': '#F1E9C9',
+    '--yellow-600': '#DBAC2C',
+    '--yellow-900': '#C47F17',
+
+    '--purple-300': '#EBE5F9',
+    '--purple-600': '#8047F8',
+    '--purple-900': '#4B2995',
+
+    '--base-title': '#272221',
+    '--base-subtitle': '#403937',
+    '--base-text': '#574F4D',
+    '--base-label': '#8D8686',
+    '--base-hover': '#D7D5D5',
+
+    '--base-button': '#E6E5E5',
+    '--base-input': '#EDEDED',
+    '--base-card': '#F3F2F2',
+}
